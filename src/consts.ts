532375ebb22a17cc77842494803a3dd91eb46330
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2026-05-04 08:50:41 +0700

    import articles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ilham Muhammad Ramdhan Gunawan';
+export const SITE_DESCRIPTION = 'This website contains Ilham\'s writings about programming, technology, and other personal interests.';
+export const ARTICLE_STATUS = ['draft', 'publish', 'archived'] as const;
