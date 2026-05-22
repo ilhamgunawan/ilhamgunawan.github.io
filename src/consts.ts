5dbe00710c27493331f8734dcfab6e5d2a12746c
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2026-05-22 16:39:46 +0700

    remove templates

--- src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Ilham Muhammad Ramdhan Gunawan';
-export const SITE_DESCRIPTION = 'This website contains Ilham\'s writings about programming, technology, and other personal interests.';
-export const ARTICLE_STATUS = ['draft', 'publish', 'archived'] as const;
