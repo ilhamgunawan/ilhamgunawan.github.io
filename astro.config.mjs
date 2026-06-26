5f974fb7c55b388525af8a6b054494c851672bbb
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2026-06-26 10:35:48 +0700

    chore: set trailingSlash false (#7)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ilhamgunawan.github.io',
+  trailingSlash: 'never',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
